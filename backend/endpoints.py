b1dac318b15638ae1bc83d77b206225dd66fb18d
Matthew Lack <matt.lack@mail.utoronto.ca>
2025-07-19 16:40:04 -0400

    wokr

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -13,3 +13,12 @@ async def analyze(
 ):
     # Check if the file is an image
     content_type = image.content_type
+    if content_type is None or content_type not in ("image/jpeg", "image/jpg", "image/png"):
+        return JSONResponse(
+            status_code=400,
+            content={"message": "No content type"},
+        )
+
+
+
+    return None
