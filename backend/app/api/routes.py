620c3371da39fb7187b5288dd73f4d4a5dcedfff
Matthew Lack <matt.lack@mail.utoronto.ca>
2025-07-20 06:06:58 -0400

    fix

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -176,7 +176,7 @@ async def analyze_satellite_data(request: SatelliteAnalysisRequest):
                 status="success",
                 image=im_id,
                 heatmap=hm_id,
-                vellum_analysis=outputs[0],
+                vellum_analysis=outputs[0].json(),
             )
         finally:
             pass
